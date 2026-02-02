20b6c581ec4f160fa6de4073fd19d1f2e179ee17
ZerosPen <137518772+AndhikaAtmaja@users.noreply.github.com>
2026-02-02 14:42:33 +0700

    Update UI
    Card Deck

--- Assets/Scripts/Card/CardDeckUI.cs
+++ Assets/Scripts/Card/CardDeckUI.cs
@@ -6,27 +6,48 @@ public class CardDeckUI : MonoBehaviour
 {
     public TextMeshProUGUI cardNameText;
     public TextMeshProUGUI staminaValueText;
+    public TextMeshProUGUI healValueText;
     public TextMeshProUGUI damageValueText;
     public Image cardIllustration;
+    public GameObject statusDamage;
+    public GameObject statusHeal;
     public GameObject statusStamina;
 
     public void SetCardDeckUI(CardSO cardData)
     {
         cardNameText.text = cardData.cardName;
-        staminaValueText.text = cardData.StaminaCost.ToString();
         cardIllustration.sprite = cardData.cardImage;
 
-        if (cardData.movementCardType == MovementCardType.Offensive)
+        if (cardData.cardType == CardType.Movement)
         {
-            //show
-            damageValueText.text = cardData.damageMovement.ToString();
-            statusStamina.SetActive(true);
-        }
-        else
-        {
-            statusStamina.SetActive(false);
+            staminaValueText.text = cardData.StaminaCost.ToString();
+            if (cardData.movementCardType == MovementCardType.Offensive)
+            {
+                //show
+                damageValueText.text = cardData.damageMovement.ToString();
+                statusDamage.SetActive(true);
+                statusStamina.SetActive(true);
+            }
+            else
+            {
+                statusDamage.SetActive(false);
+            }
         }
 
+        if (cardData.cardType == CardType.Item)
+        {
+            
+            if (cardData.itemCardType == ItemCardType.HealthPotion)
+            {
+                healValueText.text = cardData.itemHealAmount.ToString();
+                statusHeal.SetActive(true);
+            }
+            else
+            {
+                damageValueText.text = cardData.itemDamage.ToString();
+                statusDamage.SetActive(true);
+            }
+        }
     }
 
 }
